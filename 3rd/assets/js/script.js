739621bbd51ff4760d13824d73001e52a3561797
jahanabir8 <abir.opediatech@gmail.com>
2023-06-30 22:40:38 +0600

    mobilemenu update

--- 3rd/assets/js/script.js
+++ 3rd/assets/js/script.js
@@ -1,6 +1,16 @@
 const menuBar = document.querySelector('.menu__bar')
+const mobileMenu = document.querySelector('.mobile__menu')
+const crossBar = document.querySelector('.cross__bar')
 
 menuBar.addEventListener("click", function (){
-  console.log("clicked")
+    mobileMenu.classList.toggle("menu__show")
+
     }
 )
+
+crossBar.addEventListener('click', ()=>{
+  
+  if(mobileMenu.classList.contains("menu__show")){
+    mobileMenu.classList.remove('menu__show')
+  }
+})
